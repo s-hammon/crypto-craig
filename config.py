bc78dad421295b7e3fe66529cc8814ab2a92de2e
Steven Hammon <67514506+s-hammon@users.noreply.github.com>
2021-03-18 08:30:34 -0500

    Add your config settings here.

--- config.py
+++
@@ -1,8 +0,0 @@
-PREFIX = ('cc.')
-PREFIX2 = ('btc.')
-TOKEN = 'ODA2NjIxNDA5MDE4OTA0NjQ2.YBsGug.ipsWHorDAkuCNT5KcG1seORR2Mk'
-TOKEN2 = 'ODE3NTA1NjAyMTM3MjI3Mjg1.YEKfbA.vm8dpfg6rH4Mt5b_FXyf5vUUkv4'
-APP_ID = ''
-OWNERS = []
-BLACKLIST = []
-BOOT_COGS = ['cogs.prices', 'cogs.db_cog']
